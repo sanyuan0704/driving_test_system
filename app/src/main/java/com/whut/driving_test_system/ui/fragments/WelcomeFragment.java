9463a699b340e7a3157e7a33cdaf3c783ac1b77b
sanyuan <494130947@qq.com>
2020-12-08 17:13:43 +0800

    feat: add welcome

--- app/src/main/java/com/whut/driving_test_system/ui/fragments/WelcomeFragment.java
+++ app/src/main/java/com/whut/driving_test_system/ui/fragments/WelcomeFragment.java
@@ -4,24 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.whut.driving_test_system.R;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WelcomeFragment extends Fragment {
-
+    private ImageButton enterButton;
     public WelcomeFragment() {
         // Required empty public constructor
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+        enterButton = view.findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController controller = Navigation.findNavController(view);
+                controller.navigate(R.id.action_welcomeFragment_to_loginFragment);
+            }
+        });
+        return view;
     }
 }
