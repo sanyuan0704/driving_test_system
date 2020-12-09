20ad6514d73ae84f8254a462266b92434f072433
ragingbear <18123456947@zrgj.com>
2020-12-09 14:14:23 +0800

    update:ExamineeAdapter

--- app/src/main/java/com/whut/driving_test_system/ui/adapters/ExamineeAdapter.java
+++ app/src/main/java/com/whut/driving_test_system/ui/adapters/ExamineeAdapter.java
@@ -1,6 +1,51 @@
 package com.whut.driving_test_system.ui.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.whut.driving_test_system.R;
+import com.whut.driving_test_system.models.eneities.Examinee;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ExamineeAdapter extends RecyclerView.Adapter {
+public class ExamineeAdapter extends RecyclerView.Adapter<ExamineeAdapter.ViewHolder> {
+    private List<Examinee> examinees;
+
+    public ExamineeAdapter(List<Examinee> examinees) {
+        this.examinees = examinees;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_examinee, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return examinees.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
