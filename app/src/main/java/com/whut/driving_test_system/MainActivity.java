e0d690f9c6162ee439dbc80f91d12b74bf573acf
ragingbear <18123456947@zrgj.com>
2020-12-12 22:46:40 +0800

    update:models

--- app/src/main/java/com/whut/driving_test_system/MainActivity.java
+++ app/src/main/java/com/whut/driving_test_system/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
                 Navigation.findNavController(findViewById(R.id.fragment)).navigate(R.id.settingsFragment);
             }
         });
+
+        initDB();
     }
 
 
@@ -54,10 +56,18 @@ public class MainActivity extends AppCompatActivity {
         );
 
         examnieeRespository.insertExaminees(
-                new Examinee("202012120001","345678200005192349","熊大","森林驾校",0,"C1",Examinee.ExamStatus.WAIT.ordinal(),0,"u1"),
-                new Examinee("202012120001","345678200005192349","蹦蹦","森林驾校",0,"C1",Examinee.ExamStatus.WAIT.ordinal(),0,"u1"),
-                new Examinee("202012120002","345678200005194560","熊二","森林驾校",0,"C1",Examinee.ExamStatus.PASSED.ordinal(),100,"u1"),
-                new Examinee("202012120003","345678198801010002","光头强","森林驾校",1,"C1",Examinee.ExamStatus.FAILED.ordinal(),80,"u1")
+                new Examinee("202012120001", "345678200005192349", "熊大", "森林驾校", 0, "C1", Examinee.ExamStatus.WAIT.ordinal(), 0, "u1"),
+                new Examinee("202012120004", "345678200005192349", "蹦蹦", "森林驾校", 0, "C1", Examinee.ExamStatus.WAIT.ordinal(), 0, "u1"),
+                new Examinee("202012120002", "345678200005194560", "熊二", "森林驾校", 0, "C1", Examinee.ExamStatus.PASSED.ordinal(), 100, "u1"),
+                new Examinee("202012120003", "345678198801010002", "光头强", "森林驾校", 1, "C1", Examinee.ExamStatus.FAILED.ordinal(), 80, "u1")
         );
+
+        ruleRepository.insertRule(
+                new Rule("r1", "车辆行驶中骑轧车道边沿实线", "压边线", "100", true, true, "考试车辆距离道路右侧边缘线值", "0CM", null, null),
+                new Rule("r2", "车辆行驶中骑轧车道中心实线", "压中心线", "100", true, true, "考试车辆距离道路中心(双黄线、单黄线、白实线)实线值", "0CM", null, null),
+                new Rule("r3", "车速超过限速规定", "超速", "10", true, true, "考车辆行驶速度状态", "80KM/H", null, null),
+                new Rule("r4", "开转向灯小于3秒即转向", "提前转向", "10", true, true, "车辆行驶状态", "起步前，超车前，变更到快车道，变更到慢车道前，左转前，右转前，调头前，靠边停车前", "灯光开关时间", "3S")
+        );
+
     }
 }
